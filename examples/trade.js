2612195d893e2d7bc479e60387901e37de305dbe
sam <sam@freighttrust.com>
2020-11-25 06:41:48 -0800

    feat(release): 0.1.0 release
    inital github release

---
+++ examples/trade.js
@@ -0,0 +1,24 @@
+const ManifoldX = require('../lib/manifoldx');
+const keys = require('./keys');
+
+const manifoldx = new ManifoldX(keys);
+
+// manifoldx.on('report', report => {
+//   if (report.header.type_id === 4) {
+//     return;
+//   }
+//   console.log(JSON.stringify(report, null, 2));
+// });
+
+manifoldx.connect().then(() => {
+  return manifoldx.newOrder({
+    market: 'ETH-DAI',
+    is_buy: false,
+    price: '300',
+    quantity: '0.1',
+  }).result;
+}).then(order => {
+  return order.cancel();
+}).then(() => {
+  console.log('balances', manifoldx.getBalances());
+});
